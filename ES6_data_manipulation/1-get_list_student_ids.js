b60d3611175b18cb3792dede21ac6d0a9c052e96
OursBlanc42 <regniersimon@hotmail.com>
2025-04-02 07:47:42 +0200

    add task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
 export default function getListStudentsIds(obj) {
   if (typeof obj === 'object') {
-    const myArray = [];
-    for (let i = 0; i < obj.length; i += 1) {
-      myArray.push(obj[i].id);
-    }
-    return myArray;
+    return obj.map((student) => student.id);
   }
   return [];
 }

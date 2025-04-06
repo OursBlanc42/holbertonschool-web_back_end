e70f2db9e451567e486cd50c128035cdd03b22a5
OursBlanc42 <regniersimon@hotmail.com>
2025-04-06 14:33:39 +0200

    add task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // return all the promise (uploadPhoto and createUser)
-  return (
-    Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-
-      // For all returned promise, log data :
-      .then((results) => ({
-        status: results.status,
-        value: results.value || results.reason,
-      })
-      )
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason,
+    }))
   );
 }

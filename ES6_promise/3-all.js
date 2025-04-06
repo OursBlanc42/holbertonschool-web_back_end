272cff763608e6881a0a96da4594ddf8bf63be2d
OursBlanc42 <regniersimon@hotmail.com>
2025-04-06 14:37:33 +0200

    rewriting

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,18 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const myPromise = new Promise((resolve, reject) => {
-    Promise.all([uploadPhoto(), createUser()])
-      .then(([dataPhoto, dataUser]) => {
-        const myReturn = `${dataPhoto.body} ${dataUser.firstName} ${dataUser.lastName}`;
-        console.log(myReturn);
-        return myReturn;
-      })
-
-      .catch(() => {
-        reject(new Error('Signup system offline'));
-      });
-  });
-
-  return myPromise;
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([dataPhoto, dataUser]) => {
+      console.log(`${dataPhoto.body} ${dataUser.firstName} ${dataUser.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }

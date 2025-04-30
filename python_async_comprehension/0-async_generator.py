86e8c9c03f89611f928d0ecf9cbba02e58e9da7b
OursBlanc42 <regniersimon@hotmail.com>
2025-04-30 14:18:10 +0200

    try with generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ between 0 and 10. It waits for 1 second between each yield
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async_generator
 

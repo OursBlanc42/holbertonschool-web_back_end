fab2ff227ad51a391466641f3b2aefcc3ca3991b
OursBlanc42 <regniersimon@hotmail.com>
2025-05-02 15:12:00 +0200

    add typing and check all documents not only one

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,10 +3,16 @@
 Update topics of a school document based on name
 """
 
+from typing import List
+from pymongo.collection import Collection
 
-def update_topics(mongo_collection, name, topics):
-    """Updates the topics of a school based on the name"""
-    mongo_collection.update_one(
+
+def update_topics(
+        mongo_collection: Collection, name: str, topics: List[str]) -> int:
+
+    """Updates the topics of a school based on the name."""
+    result = mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
+    return result.modified_count

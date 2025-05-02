e47ea8ebb4c6d9227bac0297d38a9bafd281d9bf
OursBlanc42 <regniersimon@hotmail.com>
2025-05-02 07:38:17 +0200

    add docstrings

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -29,19 +29,37 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """
-        Dataset indexed by sorting position, starting at 0
+        """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: truncated_dataset[i] for i in range(len(truncated_dataset))
+                i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """
+        Retrieves a page of data from the indexed dataset starting from a
+        given index.
+        Ensures deletion resilience pagination, meaning it works correctly
+        even if some entries are deleted.
+        Args:
+            index (int, optional): The starting index for data retrieval.
+            Defaults to 0.
+            page_size (int, optional): The number of items to
+            retrieve per page.
+            Defaults to 10.
 
+        Returns:
+            Dict: A dictionary with the following keys:
+                - index (int): The starting index of the current page.
+                - next_index (int): The index for the next page of data.
+                - page_size (int): The number of items in the current page.
+                - data (List[List]): The list of data items for the
+                current page.
+        """
         # Retrieve the indexed dataset from the Server class
         indexed_data = self.indexed_dataset()
 

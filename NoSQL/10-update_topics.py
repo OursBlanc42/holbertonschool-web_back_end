6ec11658f5d6517bd9ff74af59bb7f933757e2b0
OursBlanc42 <regniersimon@hotmail.com>
2025-05-02 15:18:14 +0200

    add mongo collection as an object

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,15 +4,12 @@ Update topics of a school document based on name
 """
 
 from typing import List
-from pymongo.collection import Collection
 
 
-def update_topics(
-        mongo_collection: Collection, name: str, topics: List[str]) -> int:
+def update_topics(mongo_collection: object, name: str, topics: List[str]):
 
     """Updates the topics of a school based on the name."""
     result = mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
+        {"name": name}, {"$set": {"topics": topics}})
+
     return result.modified_count
